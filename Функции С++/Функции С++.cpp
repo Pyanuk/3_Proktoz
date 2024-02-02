be06b46b096fb032010d2b4b630619ddac53f5f3
Pyanuk <144242312+Pyanuk@users.noreply.github.com>
2024-02-02 19:52:33 +0300

    3pr

--- Функции С++/Функции С++.cpp
+++ Функции С++/Функции С++.cpp
@@ -1,23 +1,28 @@
-﻿#include <iostream>
+#include <iostream>
+#include <cmath>
+
 using namespace std;
 
-double krug(double radius) {
-    return 3.14 * radius * radius;
+double krug(double outerRadius, double innerRadius) {
+    return 3.14 * (outerRadius * outerRadius - innerRadius * innerRadius);
 }
 
 int main() {
+
     setlocale(LC_ALL, "Russian");
 
-    double radius_1, radius_2;
+    double outerRadius, innerRadius;
 
-    cout << "Введите второй радиус: ";
-    cin >> radius_1;
+    cout << "Введите внешний радиус кольца: ";
+    cin >> outerRadius;
 
-    cout << "Введите второй радиус: ";
-    cin >> radius_2;
+    cout << "Введите внутренний радиус кольца: ";
+    cin >> innerRadius;
 
-    double areaRing = krug(radius_1) - krug(radius_2);
-    cout << "Вывод: " << areaRing << endl;
- 
+  
+    double a = krug(outerRadius, innerRadius);
+
+    cout << "Площадь кольца: " << a << endl;
+
     return 0;
 }
